f296c6ff626055a21545164cda3b84fd6135b584
tsuttsu305 <tsuttsu305@daiwa-hotcom.com>
2012-11-17 17:15:49 +0900

    wild card support

--- src/jp/tsuttsu305/PlayerJoin.java
+++ src/jp/tsuttsu305/PlayerJoin.java
@@ -12,6 +12,7 @@ import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class PlayerJoin implements Listener {
+	public final static String AR = "\u002A";
 
 	private IPPerm ipPerm = null;
 
@@ -20,6 +21,7 @@ public class PlayerJoin implements Listener {
 		this.ipPerm =ipPerm;
 
 	}
+	
 	@EventHandler(priority=EventPriority.HIGH)
 	public void onPlayerJoin(PlayerJoinEvent event){
 		//debug code
@@ -27,58 +29,65 @@ public class PlayerJoin implements Listener {
 
 		Player joinPlayer = event.getPlayer();
 		List<String> adminPlayers = ipPerm.getAdminList();
+		
 		boolean adminTF = false;
 		for (String s:adminPlayers){
 			//debug code
 			//ipPerm.getServer().broadcastMessage(s + "J: " + joinPlayer.getName());
-
+			
 			if (s.equals(joinPlayer.getName())){
 				adminTF = true;
 				break;
 			}
-
 		}
 		if (adminTF == true){
 			//debug code
 			//ipPerm.getServer().broadcastMessage("Adminlist Found");
 
 			PermissionUser user = PermissionsEx.getUser(joinPlayer);
-			String playerIP = joinPlayer.getAddress().getHostName(); //JoinしたPlayerのIPアドレス取得
+			String[] playerIP = joinPlayer.getAddress().getHostName().split("\\."); //JoinしたPlayerのIPアドレス取得 "."で分割
 			String groupAdmin = ipPerm.getConfig().getString("adminGroup");	//Configからグループ取得 Admin
 			String groupMember = ipPerm.getConfig().getString("memberGroup"); //Member
-
 			List<String> confIP = ipPerm.getConfIP(joinPlayer); //Configのデータ取得
-
 
 			//debug code
 			//ipPerm.getServer().broadcastMessage("JoinIP: " + playerIP);
 
-			boolean perm = false;
+			boolean admin = false;
 
 			for (String s:confIP){
+				boolean[] perm = {false, false, false, false};
 				//debug code
 				//ipPerm.getServer().broadcastMessage(s);
 
-				//判定
-				if (s.equals(playerIP)){
-					//debug code
-					//ipPerm.getServer().broadcastMessage("IP True");
+				//Config側のアドレスを"."で分割
+				String[] confIPs = s.split("\\.");
 
-					perm = true;
+				//判定
+				for(int i = 0;i<=3;i++)
+				if (confIPs[i].equals(AR)){
+					perm[i] = true;
+				}else if(confIPs[i].equals(playerIP[i])){
+					perm[i] = true;
+				}
+
+				//IPが一致した場合はAdmin権限を付与。
+				if (perm[0] == true && perm[1] == true && perm[2] == true && perm[3] == true){
+					//debug code
+					//ipPerm.getServer().broadcastMessage("Admin");
+					ipPerm.logger.info("[IPPerm] " + joinPlayer.getName() + " is " + groupAdmin + " Group Now!");
+
+					String[] groups = {groupAdmin};
+					user.setGroups(groups);
+					admin = true;
 					break;
+					}
 				}
 
 
-			}
-			//IPが一致した場合はAdmin権限を付与。一致がない場合はMember権限を付与
-			if (perm == true){
-				//debug code
-				//ipPerm.getServer().broadcastMessage("Admin");
-				ipPerm.logger.info("[IPPerm] " + joinPlayer.getName() + " is " + groupAdmin + " Group Now!");
 
-				String[] groups = {groupAdmin};
-				user.setGroups(groups);
-			}else{
+			//一致がない場合はMember権限を付与
+			if (!(admin)){
 				//debug code
 				//ipPerm.getServer().broadcastMessage("Member");
 				ipPerm.logger.info("[IPPerm] " + joinPlayer.getName() + " is " + groupMember + " Group Now!");
@@ -87,7 +96,6 @@ public class PlayerJoin implements Listener {
 				user.setGroups(groups);
 			}
 		}
-
 
 		return;
 	}
